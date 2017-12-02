e4c41a48201e6c2f8eead1431ffc1f83ae77ba3b
Ariful Islam <arif3258@gmail.com>
2017-12-02 15:44:18 +0600

    score and life system

---
+++ Assets/Script/TouchManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchManager : MonoBehaviour {
+	private float initialTouchTime;
+	private float finalTouchTime;
+	private Vector2 initialTouchPos;
+	private Vector2 finalTouchPos;
+
+	private float xAxisForce;
+	private float yAxisForce;
+	private float zAxisForce;
+
+	private Vector3 userForce;
+
+	public Rigidbody egg;
+
+	private bool canSwipe = true;
+
+	void Start(){
+		
+		egg.useGravity = false;
+	}
+
+
+	//when it is touched
+	public void IsTocuh () {
+		if (canSwipe) {
+			initialTouchTime = Time.time;
+			initialTouchPos = Input.mousePosition;
+		}
+	}
+
+	// when finger is lifted
+	public void IsTouchRemoved(){
+		if (canSwipe) {
+			finalTouchTime = Time.time;
+			finalTouchPos = Input.mousePosition;
+			EggThrouw ();
+		}
+	}
+
+	private void EggThrouw(){
+	
+		xAxisForce = finalTouchPos.x - initialTouchPos.x;
+		yAxisForce = finalTouchPos.y - initialTouchPos.y;
+		zAxisForce = finalTouchTime - initialTouchTime;
+
+		userForce = new Vector3 (xAxisForce/50f , yAxisForce/40f, zAxisForce * 8f);
+		egg.useGravity = true;
+		egg.velocity = userForce;
+		//canSwipe = false;
+	}
+}
---
+++ Assets/Script/eggGame.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class eggGame : MonoBehaviour {
+	public Text score;
+	public Text lifeCount;
+
+	private int currentScore = 0;
+	private int life = 12;
+
+	private Vector3 eggPosition;
+
+	void Start() {
+		eggPosition = this.transform.position;
+	}
+		
+	void OnTriggerEnter (Collider col){
+		if (col.gameObject.tag == "score") {
+			ScoreUpdate ();
+		} else if (col.gameObject.tag == "env") {
+			eggDead ();
+		}
+	}
+
+	private void ScoreUpdate(){
+		currentScore++;
+		score.text = currentScore.ToString ();
+		eggPosition = this.transform.position;
+	}
+
+	private void eggDead() {
+		life--;
+		lifeCount.text = life.ToString ();
+		if (life <= 0)
+			gameOver ();
+		else {
+			respawn ();
+		}
+	}
+
+	private void gameOver() {
+		
+	}
+
+	private void respawn() {
+		this.transform.position = eggPosition;
+
+	}
+}
